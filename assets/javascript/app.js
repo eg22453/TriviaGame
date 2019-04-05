f2d33a79b72cb60565034c193ee0a011d34cefdd
Steven Garza (eg22453) <stevenmacgarza@gmail.com>
2019-04-05 17:53:43 -0500

    add a start button with hide and show functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,22 @@
-
-
-let timeremaining = 10 //should this be in seconds?
+let timeremaining = 10; //should this be in seconds?
 let rightanswers = 0;
 let wronganswers = 10;
+var Interval;
 
-var Interval = setInterval(startcountdown, 1000);
+
+//var Interval = setInterval(startcountdown, 1000);
 // Run this code at every interval
-
-
 //IMPORTANT in the above line of code i thought startcountdown() was correct
 // but it only ran once, when I removed the () it decrememented by a 1 second countdown like expected
-document.on
+
+
+
+$("#startbutton").click(function () {
+    $("#startbutton").hide()
+    Interval = setInterval(startcountdown, 1000);
+    
+
+})
 function startcountdown() {
     if (timeremaining === -1) {
         $("input:checked").each(function () {
@@ -18,7 +24,7 @@ function startcountdown() {
                 rightanswers++;
                 wronganswers--;
             }
-           
+
         })
         clearInterval(Interval) //This will stop the code from running every 1 second, stops the countdown
         $("#quizbox").hide()
